1d4ee53db921cfa5750b610e816102ea386cf9b2
Thiago Serrano <aceman6711@gmail.com>
2019-02-15 23:55:55 -0200

    Enter

--- src/pages/more-info-tasks/more-info-tasks.ts
+++ src/pages/more-info-tasks/more-info-tasks.ts
@@ -11,7 +11,7 @@ export class MoreInfoTasksPage {
     id          : any;
     pNome       : string;
     NewTask     : any;
-    tkCorrigido : Boolean = false;
+    tkConcluida : Boolean = false;
     tkPendente  : Boolean = false;
     tkParada    : Boolean = false;
 
@@ -21,7 +21,7 @@ export class MoreInfoTasksPage {
       switch (this.id) {
           case 1 : { this.tkPendente  = true; this.pNome='Thiago Serrano';      break; }
           case 2 : { this.tkParada    = true; this.pNome='Cesar Lopes';         break; }
-          case 3 : { this.tkCorrigido = true; this.pNome='Leonardo Ikehara';    break; }
+          case 3 : { this.tkConcluida = true; this.pNome='Leonardo Ikehara';    break; }
           case 4 : { this.tkPendente  = true; this.pNome='Thor Odinson';        break; }
       };
       if(this.id=='NewTask'){
@@ -32,13 +32,24 @@ export class MoreInfoTasksPage {
   fechaModal(){
     this.navCtrl.pop();
   }
-  teste(){
+  zeraVariavel(){
+    this.tkPendente  =false;
+    this.tkConcluida =false;
+    this.tkParada    = false;
+  }
+  teste(tkStatus : String){
     const toast = this.toastCtrl.create({
                            message  : 'OOOOOIIIII!!!',
                            duration : 2000,
                            position : 'top'
                     });
     toast.present();
-    console.log();
+    console.log(tkStatus);
+    switch (tkStatus) {
+        case '1' : { this.zeraVariavel(); this.tkPendente  = true; break; }
+        case '2' : { this.zeraVariavel(); this.tkParada    = true; break; }
+        case '3' : { this.zeraVariavel(); this.tkConcluida = true; break; }
+        case '4' : { this.zeraVariavel(); this.tkPendente  = true; break; }
+    }
   }
 }
